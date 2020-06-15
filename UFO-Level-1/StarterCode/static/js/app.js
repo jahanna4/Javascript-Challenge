0e5620952b4fca02652baf7c56007d6fe5963570
jahanna4 <61722728+jahanna4@users.noreply.github.com>
2020-06-15 02:08:42 -0400

    Updated
    Updated

--- UFO-Level-1/StarterCode/static/js/app.js
+++ UFO-Level-1/StarterCode/static/js/app.js
@@ -18,97 +18,27 @@ function TableDefault() {
 
 TableDefault();
 
-// 1. SELECT INPUT
-// var input = d3.select(".form-control").attr("placeholder")
 
-// 2. COMPARE INPUT TO DATE COLUMN
-// var tablerows = document.getElementsByTagName("tr")[i]
+// var searchinput = d3.select(".form-control").attr("placeholder");
+// var tablerows = document.getElementsByTagName("tr");
 
-// 3. RETURN ROWS
+filtertable.on("click", filtsel);
 
-// 4. REPLACE DATA VARIABLE WITH NEW ROWS
+function filtsel() {
+    d3.event.preventdefault();
 
-// 5. UPDATE TABLEDEFAULT FUNCTION
+    var entry = d3.select("#datetime");
+    var entryval = entry.property("value");
+    var filt = tableData.filter(row => row.datetime === entryval);
+    console.log(filt);
 
-// filtertable.on("click", datesearch);
+    filt.forEach((ufodata) => {
+        var newrow = tableselect.append("tr");
+        Object.entries(ufodata).forEach(([key,value]) => {
+            var newcell = newrow.append("td");
+            newcell.text(value);
+        });
+    });
+}
 
-// var input = d3.select(".form-control").attr("placeholder");
-// // console.log(input);
-// var trow = d3.select("tbody>tr");
-// var rowvalue = d3.select("tbody>tr>td").text();
-
-// 
-// filtertable.on("click", testingfin);
-
-// Logic: When the button is clicked, grab the text in the input box, search the date column for matching text and return the whole row
-// var searchterms = d3.event.target.value;
-var searchinput = d3.select(".form-control").attr("placeholder");
-var tablerows = document.getElementsByTagName("tr");
-var newdata = [];
-
-function datesearch(event) {
-    searchtext = d3.event.target.value;
-    for (var i = 1; i < tablerows.length; i++) {
-        var tableDates = tablerows[i].getElementsByTagName("td")[0];
-            datetext = tableDates.innerText;
-            if (datetext = searchtext) {
-                // console.log(tablerows[i]);
-                newdata.push(tablerows[i]);
-                // return newdata;
-            }
-        }
-    }
-
-filtertable.on("click", datesearch);
-
-// function newtable() {
-//     newdata.forEach((ufodata) => {
-//         var newrow = tableselect.append("tr");
-//         Object.entries(ufodata).forEach(([key,value]) => {
-//             var newcell = newrow.append("td");
-//             newcell.text(value);
-//         });
-//     });
-// }
-
-// newtable();
-// datesearch();
-
-//         // d3.select("tbody>tr>td").text()
-//         output = []
-//         var searchterms = document.getElementsById("datetime");
-//         // d3.select("#datetime")
-//         // if (tableDates = searchinput) {
-//         //     output.append(tablerows[i])
-//         // };
-//         if (tableDates) {
-//             if (searchterms.length == 0)
-//         }
-//     };
-//     return output;
-//     };
-
-// function datesearch() {
-//     var inputs = document.getElementById("datetime");
-//     // var filter = inputs.value.toUpperCase();
-//     var table = document.getElementById("ufo-table");
-//     console.log(table)
-//     var row = table.getElementsByTagName("tr");
-//     console.log(row)
-    // for (i=0; i<row.length; i++) {
-    //     dataline = row[i].getElementsByTagName("td")[0];
-    //     console.log(dataline);
-    //     if (dataline) {
-    //         txtValue = dataline.textContent || dataline.innerText;
-    //         if (txtValue.indexOf(inputs) > -1) {
-    //             row[i].style.display = "none";
-    //         }
-    //     }
-    
-    // }
-    // var dataline = row[i].getElementsByTagName("td")[0];
-    // var txtValue = dataline.textContent || dataline.innerText;
-    // }
-
-// filtertable.on("click", datesearch);
-
+// Note: filtered rows append to bottom of existing table.
